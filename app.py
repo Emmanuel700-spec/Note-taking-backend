df99f9a2ba359e0d622f58e67c1c692037cedf45
Emmanuelronoh <eronoh036@gmail.com>
2024-10-23 23:04:55 +0300

    first commit

--- app.py
+++ app.py
@@ -1,50 +1,45 @@
-from flask import Flask, session, jsonify, request
+import os  # Import os to access environment variables
+from models import db
+from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
-from models import db, User  
-from routes import api_bp
-import logging
 from flask_bcrypt import Bcrypt
-from flask_mail import Mail
+
+# Import the blueprint from routes.py
+from routes import api_bp
 
 def create_app():
     """Create and configure the Flask application."""
     app = Flask(__name__)
 
-    # Load configuration
-    app.config.from_object('config.Config')  
-    app.config['SECRET_KEY'] = 'f83e412280ce4cb1cf021835276930269087e08fc4251236'  
-    app.config['SESSION_COOKIE_NAME'] = 'your_session_cookie'  
-    app.config['SESSION_TYPE'] = 'filesystem' 
+    # Load configuration from environment variables
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f"postgresql://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@"
+        f"{os.environ.get('DB_HOST')}/{os.environ.get('DB_NAME')}"
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, to suppress warnings
+
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your-default-secret-key')  # Load from env or use a default
+    app.config['SESSION_COOKIE_NAME'] = 'your_session_cookie'
+    app.config['SESSION_TYPE'] = 'filesystem'
 
     # Initialize CORS with specific origin
     CORS(app, supports_credentials=True, resources={r"/api/*": {"origins": "*"}})
 
-    # Initialize extensions
+    
     db.init_app(app)
     bcrypt = Bcrypt(app)
     migrate = Migrate(app, db)
-    mail = Mail(app)
 
-    # Register the API blueprint
+    
     app.register_blueprint(api_bp, url_prefix='/api')
 
-    # Add a simple homepage route
+    
     @app.route('/')
     def home():
         return "Welcome to the Note Taking App! The app is currently running."
-    
+
     with app.app_context():
         db.create_all()  
 
-    return app  
-
- 
-
-if __name__ == '__main__':
-    # Run locally using flask run
-    app = create_app()
-    app.run()
-else:
-    #run using gunucorn gunicorn -b 0.0.0.0:5000 app:gunicorn_app
-    gunicorn_app = create_app()
+    return app
